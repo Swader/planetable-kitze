c7bc9375de95d14b8732b246352bcc436aa6bb61
Bruno Skvorc <bruno@skvorc.me>
2026-05-16 18:20:02 +0200

    Fix Kitze content image scaling

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-buildNumber=`git rev-list --count main`
+buildNumber=`git rev-list --count HEAD`
 jq ".buildNumber = $buildNumber" template.json > template.json.tmp && mv template.json.tmp template.json && git add template.json
